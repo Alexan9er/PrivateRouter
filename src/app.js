8c7f78bfc0a99a19113106cdac42fb2dce181220
Alexander Kleymonov <Kleimionov.Alexander@gmail.com>
2019-09-08 12:16:31 +0300

    Added private route.

--- src/app.js
+++ src/app.js
@@ -2,13 +2,23 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { routes } from './routes';
 import NavigationBar from './components/navigationBar';
+import PrivateRoute from './containers/privateRoute';
 
 const App = () => {
   const renderSwitch = () => (
     <Switch>
       {routes.map((route) => {
         const { path, isExact, component, isPrivate } = route;
-
+        if (isPrivate) {
+          return (
+            <PrivateRoute
+              key={path}
+              exact={isExact}
+              path={path}
+              component={component}
+            />
+          );
+        }
         return (
           <Route key={path} exact={isExact} path={path} component={component} />
         );
